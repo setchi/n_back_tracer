44018f86c22b6c12a35cd97b55a217909a391e10
setchi <ri_xer@yahoo.co.jp>
2014-12-08 14:19:02 +0900

    軽くリファクタ

--- Assets/Scripts/PatternGenerator.cs
+++ Assets/Scripts/PatternGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PatternGenerator : MonoBehaviour {
-
+	
 	public int FieldWidth {
 		set { fieldWidth = value; }
 	}
@@ -12,7 +12,7 @@ public class PatternGenerator : MonoBehaviour {
 	}
 	int fieldWidth;
 	int fieldHeight;
-
+	
 	Vector2[] directions = {
 		Vector2.up,
 		Vector2.right,
@@ -24,13 +24,13 @@ public class PatternGenerator : MonoBehaviour {
 		int x = fieldWidth + 2, y = fieldHeight + 2;
 		int fieldSize = y * x + x;
 		
-		int[] fieldArr = SentinelsSetOfWall (new int[fieldSize], x, y);
+		int[] fieldArr = SetSentinelsOfWall (new int[fieldSize], x, y);
 		fieldArr = PatternWriteToField (fieldArr, ignorePattern, x, y);
 		// ArrPrint (testArr, x, y);
 		
 		Stack<int> patternStack = new Stack<int> ();
-
-		while (!DepthFirstSearch (
+		
+		while (!PatternBuildDFS (
 			fieldArr,
 			new Vector2(Random.Range(0, fieldWidth - 1), Random.Range(0, fieldHeight - 1)),
 			Random.Range(0, 3),
@@ -38,7 +38,7 @@ public class PatternGenerator : MonoBehaviour {
 		));
 		
 		return new List<int>(patternStack.ToArray ());
-
+		
 		/**
 		int row = 0;
 		List<int> pattern = new List<int> ();
@@ -65,52 +65,48 @@ public class PatternGenerator : MonoBehaviour {
 	}
 	/**/
 	
-	bool DepthFirstSearch(int[] field, Vector2 current, int direction, ref Stack<int> stack) {
+	bool PatternBuildDFS(int[] field, Vector2 current, int direction, ref Stack<int> stack) {
 		if (stack.Count == 4)
 			return true;
-
+		
 		if (IsWall(field, current))
 			return false;
 		
 		stack.Push (fieldWidth * (int)current.y + (int)current.x);
 		
 		// 後ろは見ない
-		int[] notBackIndexes = {0, 1, 3};
-		for (int i = 0, l ; i < 2; i++) {
-			int notBackIndex = notBackIndexes[Random.Range(0, notBackIndexes.Length - 1)];
+		List<int> notBackIndexes = new List<int>(new int[] {0, 1, 3});
+		for (int i = 0, end = notBackIndexes.Count ; i < end; i++) {
+			int notBackIndex = notBackIndexes[Random.Range(0, end - i - 1)];
 			int newDirection = LoopIndex (direction + notBackIndex, directions.Length - 1);
-
+			
 			// 進めるところまで進む
-			if (DepthFirstSearch (
-				field,
-				current + directions[newDirection],
-				newDirection,
-				ref stack
-			)) {
+			if (PatternBuildDFS(field, current + directions[newDirection], newDirection, ref stack)) {
 				return true;
 			}
+			notBackIndexes.Remove(notBackIndex);
 		}
 		
 		stack.Pop ();
 		return false;
 	}
-
+	
 	bool IsWall(int[] fieldArr, Vector2 current) {
 		return fieldArr [((int)current.y + 1) * (fieldWidth + 2) + (int)current.x + 1] == 1;
 	}
-
+	
 	int[] PatternWriteToField(int[] arr, List<int> pattern, int endX, int endY) {
 		foreach (int i in pattern) {
 			int x = i % (endX - 2);
 			int y = i / (endY - 2);
-
+			
 			arr[y * endX + x + 1] = 1;
 		}
-
+		
 		return arr;
 	}
 	
-	int[] SentinelsSetOfWall(int[] arr, int endX, int endY) {
+	int[] SetSentinelsOfWall(int[] arr, int endX, int endY) {
 		for (int y = 0; y < endY; y++)
 			for (int x = 0; x < endX; x++)
 				arr[y * endX + x] = (
@@ -119,7 +115,7 @@ public class PatternGenerator : MonoBehaviour {
 					x == endX - 1 ||
 					y == endY - 1
 				) ? 1 : 0;
-
+		
 		return arr;
 	}
 	
