cf1cb2c623709b08da988143b68060da9b446e27
setchi <ri_xer@yahoo.co.jp>
2014-12-24 16:22:54 +0900

    みづらかったのでタイルの見た目を戻した。

--- Assets/Scripts/Main/Tile.cs
+++ Assets/Scripts/Main/Tile.cs
@@ -20,8 +20,8 @@ public class Tile : MonoBehaviour {
 		gameController = GameObject.Find ("Tiles").GetComponent<GameController>();
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 		lineRenderer = GetComponentInChildren<LineRenderer>();
-		// lineRenderer.SetWidth (0.13f, 0.13f);
-		lineRenderer.SetWidth (0.06f, 0.06f);
+		lineRenderer.SetWidth (0.13f, 0.13f);
+		// lineRenderer.SetWidth (0.06f, 0.06f);
 		// 線のスタート位置は常にタイルの中心
 		lineRenderer.SetPosition(0, Vector3.zero);
 	}
@@ -45,8 +45,8 @@ public class Tile : MonoBehaviour {
 		TweenPlayer.Play(gameObject, new Tween(1f).ScaleTo(gameObject, Vector3.one, EaseType.linear));
 		// TweenPlayer.Play(gameObject, new Tween(0.4f).ScaleTo(gameObject, Vector3.one * 1.2f, Vector3.one, EaseType.easeOutBounce));
 		TweenPlayer.Play(gameObject,
-		    new Tween(0.3f)
-		    	.ValueTo(Vector3.one, new Vector3(0, 1, 0), EaseType.linear, value => UpdateColor(value.x, value.y, value.z)),
+		    //new Tween(0.3f)
+		    //	.ValueTo(Vector3.one, new Vector3(0, 1, 0), EaseType.linear, value => UpdateColor(value.x, value.y, value.z)),
 			new Tween(1f)
 				.ValueTo(new Vector3(0, 1, 0), defaultColor, EaseType.linear, value => UpdateColor(value.x, value.y, value.z))
 				.Complete(CompleteEffect)
@@ -57,7 +57,7 @@ public class Tile : MonoBehaviour {
 		TweenPlayer.CancelAll(gameObject);
 		TweenPlayer.Play(gameObject,
 			new Tween(0.4f)
-				.ScaleTo(gameObject, Vector3.one * 1.4f, Vector3.one, EaseType.easeOutBounce)
+				.ScaleTo(gameObject, Vector3.one * 1.3f, Vector3.one, EaseType.easeOutBounce)
 				.ValueTo(Vector3.one, new Vector3(0, 1, 1), EaseType.easeOutBounce, value => UpdateColor(value.x, value.y, value.z))
 		);
 	}
