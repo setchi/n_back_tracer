b66916b524f4c6477a36df032bf815f5f4555191
setchi <ri_xer@yahoo.co.jp>
2015-01-25 17:43:08 +0900

    タッチ時のエフェクト仮実装

---
+++ Assets/Scripts/EmitEffectInTouch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+
+public class EmitEffectInTouch : MonoBehaviour {
+	public GameObject effect;
+
+	[Header("ヒエラルキー上の並び順")]
+	public int siblingIndex;
+
+	void Update() {
+		/**/
+		for (int i = 0, l = Input.touchCount; i < l; i++) {
+			var touch = Input.GetTouch(i);
+			
+			if (touch.phase == TouchPhase.Began) {
+				Emit(touch.position);
+			}
+		}
+		/*/
+
+		if (Input.GetMouseButtonDown(0)) {
+			Emit(Input.mousePosition);
+		}
+		/**/
+	}
+
+	void Emit(Vector3 pos) {
+		var obj = (GameObject) Instantiate(effect, Vector3.zero, Quaternion.identity);
+		obj.transform.SetParent(gameObject.transform);
+		obj.transform.position = pos;
+		obj.transform.SetSiblingIndex(siblingIndex);
+	}
+}
---
+++ Assets/Scripts/TouchEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+
+public class TouchEffect : MonoBehaviour {
+	public GameObject[] elements;
+	float animTime = 0.4f;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Emit());
+	}
+	
+	IEnumerator Emit() {
+		foreach (var obj in elements) {
+			obj.transform.SetParent(gameObject.transform);
+			obj.transform.position = transform.position;
+			Animate(obj);
+			yield return new WaitForSeconds(0.1f);
+		}
+		
+		yield return new WaitForSeconds(animTime);
+		DestroyObject(gameObject);
+	}
+	
+	void Animate(GameObject obj) {
+		var from = Vector3.zero;
+		var to = Vector3.one * 2f;
+
+		TweenPlayer.Play(gameObject, new Tween(animTime).ValueTo(from, to, EaseType.easeOutSine, pos => {
+			obj.transform.localScale = pos;
+			obj.GetComponent<Image>().color = new Color(1, 1, 1, (2 - pos.x) / 2f);
+
+		}));
+	}
+}
