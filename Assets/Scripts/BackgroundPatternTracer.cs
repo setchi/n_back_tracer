b7ad51f81fa87c4d36aacf7502e1df402a3c6fb9
setchi <ri_xer@yahoo.co.jp>
2015-04-08 22:27:28 +0900

    Refactor zip args

--- Assets/Scripts/BackgroundPatternTracer.cs
+++ Assets/Scripts/BackgroundPatternTracer.cs
@@ -27,10 +27,10 @@ public class BackgroundPatternTracer : MonoBehaviour {
 		};
 
 		Observable.Timer (TimeSpan.Zero, TimeSpan.FromSeconds (0.7f))
-			.Zip(patterns.ToObservable(), (a, b) => b).Repeat().Subscribe(pattern => {
+			.Zip(patterns.ToObservable(), (_, p) => p).Repeat().Subscribe(pattern => {
 
 			var traceStream = Observable.Timer (TimeSpan.Zero, TimeSpan.FromSeconds (0.1f))
-				.Zip(pattern.ToObservable(), (a, b) => tiles[b]);
+				.Zip(pattern.ToObservable(), (_, i) => tiles[i]);
 
 			traceStream
 				.Do(tile => tileEffectEmitters[pattern.Peek() % 2](tile))
